225da72e11a4d4275be9ce772619e95beb96e2cf
zohaib <zohaibansari339@gmail.com>
2026-02-15 20:29:17 +0500

    feat: update navbar component to accept theme and setTheme props

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const navbar = () => {
+const navbar = ({theme, setTheme}) => {
   return (
-    <div>
+    <div className='flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
       
+      <img src="{theme === 'dark' ? }" alt="" />
     </div>
   )
 }
