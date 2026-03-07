dbe189b765049b7308dfb39d313084812a1e41ec
zohaib <zohaibansari339@gmail.com>
2026-03-07 22:36:22 +0500

    feat: add TrustedBy component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'  
 import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
 
 function App() {
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme'): 'light')
@@ -9,6 +10,7 @@ function App() {
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />   
       <Hero/>
+      <TrustedBy/>
     </div>
   )
 }
